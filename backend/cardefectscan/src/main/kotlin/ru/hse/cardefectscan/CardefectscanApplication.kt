f2b83b1ed25f6db199b27b0b39a351992e4dcea7
stas <orobtsovv@gmail.com>
2025-02-11 22:29:30 +0300

    initial commit

---
+++ backend/cardefectscan/src/main/kotlin/ru/hse/cardefectscan/CardefectscanApplication.kt
@@ -0,0 +1,11 @@
+package ru.hse.cardefectscan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardefectscanApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardefectscanApplication>(*args)
+}
---
+++ backend/cardefectscan/src/test/kotlin/ru/hse/cardefectscan/CardefectscanApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.hse.cardefectscan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardefectscanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
