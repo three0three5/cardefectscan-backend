484ccadd419b10152148d3b84ae41746b4ec272a
stas <orobtsovv@gmail.com>
2025-03-11 23:31:35 +0300

    add metadata schema

--- backend/cardefectscan/liquibase/changelog/v2/add-columns-rollback.sql
+++ backend/cardefectscan/liquibase/changelog/v2/add-columns-rollback.sql
@@ -1,1 +1,2 @@
 ALTER TABLE image_request DROP COLUMN IF EXISTS "created_at";
+ALTER TABLE image_request DROP COLUMN IF EXISTS description;
--- backend/cardefectscan/liquibase/changelog/v2/add-columns.sql
+++ backend/cardefectscan/liquibase/changelog/v2/add-columns.sql
@@ -1,3 +1,7 @@
 ALTER TABLE image_request
 ADD COLUMN IF NOT EXISTS
 "created_at" TIMESTAMP NOT NULL DEFAULT now();
+
+ALTER TABLE image_request
+    ADD COLUMN IF NOT EXISTS
+        "description" TEXT;
